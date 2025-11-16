9d10c4105267d86d6a1225f5c669a082ec1da0ac
fitvia <fitvia@fitvias-air.lan>
2025-11-16 17:11:36 +0100

    added password input textbox to landing page

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import './LandingPage.css';
 import fotosergi from '../assets/images/sergi_peque.jpeg';
@@ -7,6 +7,16 @@ import fotopaula from '../assets/images/paula_peque1.jpeg';
 
 const LandingPage: React.FC = () => {
   const navigate = useNavigate();
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+
+  const handleEnter = () => {
+    if (password === "ps2026") {    
+      navigate('/laboda');
+    } else {
+      setError('ep! contrasenya incorrecta');
+    }
+  };
 
   return (
     <div className="landing-container">
@@ -22,9 +32,21 @@ const LandingPage: React.FC = () => {
         Paula &<br />Sergi
       </h2>
       <h1 className="title">Ens casem!</h1>
-        <button className="enter-button" onClick={() => navigate('/laboda')}>
+
+        <input
+          type="password"
+          className="password-input"
+          placeholder="Introdueix la contrasenya"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        {error && <p className="error-text">{error}</p>}
+
+        <button className="enter-button" onClick={handleEnter}>
           ENTRA
         </button>
+
       </div>
     </div>
   );
